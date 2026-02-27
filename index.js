1591e9daeb75891c863710dc2756d6966a1c105b
Spanky45 <245573075+Spanky45@users.noreply.github.com>
2026-02-26 19:01:25 -0600

    trying to figure out grid spacing math.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const gridContainer = document.querySelector('.grid-container');
 
 function createGrid(tiles) {
-    for (let i = 0; i < (tiles); i++) {
+    for (let i = 0; i < tiles; i++) {
         const div = document.createElement('div');
+        div.classList.add('grid-tile');
         gridContainer.appendChild(div);
     }
 }
 
-createGrid(5);
+createGrid(400);
