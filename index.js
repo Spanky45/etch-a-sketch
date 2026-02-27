0bfcbe3f740f373036938d67e555e04875a39618
Spanky45 <245573075+Spanky45@users.noreply.github.com>
2026-02-27 14:27:20 -0600

    created the logic that adds the number of grid tiles to the grid container

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const gridContainer = document.querySelector('.grid-container');
+const gridLength = 1000
+const tileMargin = 2;
 
-function createGrid(tiles) {
-    for (let i = 0; i < tiles; i++) {
+function createGrid(tilesPerSide) {
+
+    const tileSize = gridLength / tilesPerSide - (tileMargin * 2);
+
+    for (let i = 0; i < tilesPerSide ** 2; i++) {
         const div = document.createElement('div');
         div.classList.add('grid-tile');
+        div.style.width = `${tileSize}px`;
+        div.style.height = `${tileSize}px`;
+        div.style.margin = `${tileMargin}px`;
         gridContainer.appendChild(div);
     }
 }
 
-createGrid(400);
+createGrid(16);
+
+// function poop(tileNumber) {
+
+// }
